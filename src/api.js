7fdd1db738eab2cf4660dc1714e9a53f121c517b
KjOHnBaggins <95200849+KjOHnBaggins@users.noreply.github.com>
2022-11-24 11:33:10 +0630

    React weather completed.

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a1b059322cmsh5ad1e70806e094ap16eb27jsn34a43691a01a",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "65ef6ae9d36a3643a218cc6e396efc8a";
